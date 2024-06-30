0545ce78e0718c7823735be222580f2875289cb2
TeenKanisorn <kanisornp.dev@gmail.com>
2024-06-30 16:41:12 +0700

    Basic React

--- MyApplication/react-basic/src/App.js
+++ MyApplication/react-basic/src/App.js
@@ -3,23 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>บัญชีรายรับ-รายจ่าย</h1>
+      <p>ข้อมูลบัญชีในแต่ละวัน</p>
+      <ul>
+        <li>ค่าเดินทาง <span>-200</span></li>
+        <li>เงินเดือน <span>20000</span></li>
+        <li>ค่าอาหาร <span>7000</span></li>
+      </ul>
+    </>
+  )
 }
 
 export default App;
---
+++ MyApplication/react-basic/src/component/HelloComponent.js
@@ -0,0 +1,5 @@
+function HelloComponent(){
+    return <h1>External Component</h1>
+}
+
+export default HelloComponent
