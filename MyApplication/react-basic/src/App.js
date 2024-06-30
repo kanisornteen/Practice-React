dc8ef9ac9349031b410458475940cc9ade9c8b79
TeenKanisorn <kanisornp.dev@gmail.com>
2024-06-30 16:50:08 +0700

    Update App.js
    Update Nested Component

--- MyApplication/react-basic/src/App.js
+++ MyApplication/react-basic/src/App.js
@@ -1,16 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Title = ()=> <h1>บัญชีรายรับ-รายจ่าย</h1>
+const Description = ()=> <p>ข้อมูลบัญชีในแต่ละวัน</p>
+const Transaction = ()=> {
   return (
     <>
-      <h1>บัญชีรายรับ-รายจ่าย</h1>
-      <p>ข้อมูลบัญชีในแต่ละวัน</p>
       <ul>
         <li>ค่าเดินทาง <span>-200</span></li>
         <li>เงินเดือน <span>20000</span></li>
         <li>ค่าอาหาร <span>7000</span></li>
       </ul>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <Title />
+      <Description />
+      <Transaction />
     </>
   )
 }
