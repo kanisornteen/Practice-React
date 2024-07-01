ef5a8ef88fc8dbf6e27d46d8c9f21ff9cf7b2393
TeenKanisorn <kanisornp.dev@gmail.com>
2024-07-01 23:01:55 +0700

    Update FromComponent.js

--- MyApplication/react-basic/src/component/FromComponent.js
+++ MyApplication/react-basic/src/component/FromComponent.js
@@ -1,16 +1,27 @@
 import './FromComponent.css'
 
 const FromComponent = ()=> {
+    const inputTitle = (event)=> {
+        console.log(event.target.value);
+    }
+    const inputAmount = (event)=> {
+        console.log(event.target.value);
+    }
+    const saveItem = (event)=> {
+        event.preventDefault();
+        console.log("บันทึกแล้ว");
+    }
+
     return (
         <>
-            <form>
+            <form onSubmit={saveItem}>
                 <div className="from-control">
                     <label>ชื่อรายการ</label>
-                    <input type="text" placeholder="ระบุชื่อรายการ"></input>
+                    <input type="text" placeholder="ระบุชื่อรายการ" onChange={inputTitle}></input>
                 </div>
                 <div className="from-control">
                     <label>จำนวนเงิน</label>
-                    <input type="text" placeholder="ระบุจำนวนเงิน"></input>
+                    <input type="text" placeholder="ระบุจำนวนเงิน" onChange={inputAmount}></input>
                 </div>
                 <div>
                     <button type="submit" className="btn">เพิ่มรายการ</button>
