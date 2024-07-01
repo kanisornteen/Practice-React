1335a1246e3c4761be1495bfe8f5db67aa35f878
TeenKanisorn <kanisornp.dev@gmail.com>
2024-07-01 13:57:29 +0700

    React Props

--- MyApplication/react-basic/src/component/Items.js
+++ MyApplication/react-basic/src/component/Items.js
@@ -1,11 +1,19 @@
-const Items = ()=> {
-    const title = "เงินเดือน";
-    const amount = 23000
+import PropTypes from 'prop-types';
+
+// อ่าน props ได้ที่โฟลเดอร์ How to Create React and Learn React - 3
+const Items = (props)=> { // จะเขียนแบบนี้ก็ได้ const Items = ({title, amount})=> {
+    const {title, amount} = props
     return (
         <>
             <li>{title} <span>{amount}</span></li>
         </>
     );
+}
+
+// อ่าน props-type ได้ที่โฟลเดอร์ How to Create React and Learn React - 5
+Items.protoTypes = {
+    title:PropTypes.string.isRequired,
+    amount:PropTypes.number.isRequired
 }
 
 export default Items
--- MyApplication/react-basic/src/component/Transaction.js
+++ MyApplication/react-basic/src/component/Transaction.js
@@ -1,14 +1,20 @@
 import Items from "./Items";
 import './Transaction.css'
+import { v4 as uuidv4 } from 'uuid';
 
 const Transaction = ()=> {
+    const data = [
+        {title: "เงินเดือน", amount:"23000"},
+        {title: "ค่าขนม", amount:"เงินเดือน"},
+        {title: "ค่าเกม", amount:"1000"},
+        {title: "ค่าไฟ"}
+    ]
+
     return (
         <ul className="container-item">
-            <Items />
-            <Items />
-            <Items />
-            <Items />
-            <Items />
+            {data.map(element => {
+                return <Items title={element.title} amount={element.amount} key={uuidv4()} /> // หรือจะเขียนแบบนี้ก็ได้ในกรณีที่ชื่อ property ของ component เหมือนกับชื่อ property ของข้อมูลที่ส่งมา return <Items {...element}/>
+            })}
         </ul>
     );
 }
