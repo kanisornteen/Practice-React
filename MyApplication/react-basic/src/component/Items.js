6eb50dba179255c8c72b526da2d6b997df65c7a1
TeenKanisorn <kanisornp.dev@gmail.com>
2024-07-02 16:28:58 +0700

    Add Items Style

--- MyApplication/react-basic/src/component/Items.js
+++ MyApplication/react-basic/src/component/Items.js
@@ -1,11 +1,14 @@
 import PropTypes from 'prop-types';
+import './Items.css'
 
 // อ่าน props ได้ที่โฟลเดอร์ How to Create React and Learn React - 3
 const Items = (props)=> { // จะเขียนแบบนี้ก็ได้ const Items = ({title, amount})=> {
     const {title, amount} = props
+    const status = amount < 0 ? "expense" : "income"
+    const symbol = amount < 0 ? "-" : "+"
     return (
         <>
-            <li>{title} <span>{amount}</span></li>
+            <li>{title} <span className={status}>{symbol}{Math.abs(amount)}</span></li>
         </>
     );
 }
